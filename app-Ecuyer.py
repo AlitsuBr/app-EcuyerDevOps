51c53f11bf04649f5af3aa844b1308f1d15b392e
AlitsuBr <gregorycarvalho3@gmail.com>
2025-05-18 15:41:16 +0200

    video

--- app-Ecuyer.py
+++ app-Ecuyer.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Bonjour FLASK CECI EST Ecuyer, LOL !"
+    return "Bonjour FLASK CECI EST Ecuyer, OMEGA LOL !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
