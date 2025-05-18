bbf941cc20bd5fbb0172694f742f294ac3e9acfb
AlitsuBr <gregorycarvalho3@gmail.com>
2025-05-18 15:33:59 +0200

    1st amend

--- app-Ecuyer.py
+++ app-Ecuyer.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Bonjour, ceci est l'app Flask de Ecuyer !"
+    return "Bonjour, ceci est l'app Flask de Gregory Ecuyer !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
